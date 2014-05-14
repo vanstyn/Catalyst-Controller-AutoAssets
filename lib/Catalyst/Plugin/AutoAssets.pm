45112974175698607dc5e8eb6538daa1c273bfce
Henry Van Styn <vanstyn@intellitree.com>
2014-05-14 14:04:33 -0400

    Now hook after instead of before setup_components
    The plugin code was incorrectly hooking before setup_components instead
    of after. This was causing controller components to get built twice. Now
    hooks after...

--- lib/Catalyst/Plugin/AutoAssets.pm
+++ lib/Catalyst/Plugin/AutoAssets.pm
@@ -19,7 +19,7 @@ after setup_finalize => sub {
   $c->log->autoflush(0) if $c->log->can('autoflush');
 };
 
-before 'setup_components' => sub { (shift)->inject_asset_controllers(@_) };
+after 'setup_components' => sub { (shift)->inject_asset_controllers(@_) };
 
 sub inject_asset_controllers {
   my $c = shift;
